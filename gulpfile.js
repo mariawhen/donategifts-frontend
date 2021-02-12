a07bf3be7e5c5cc0edb63783ada4df948b4c7317
Marco Schuster <lettucekiing@gmail.com>
2021-02-12 11:00:39 +0100

    fix: cleanup (#10)
    * fix: cleanup
    
    * fix: exlude draft pull requests on actions
    
    * refactor: remove Home.module.scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const { parallel, task } = require('gulp');
 const del = require('del');
 
-task('cleanModules', () => del(['node_modules', '**/node_modules'], { force: true, dot: true }));
+task('cleanModules', () => del(['./node_modules', '**/node_modules'], { force: true, dot: true }));
 
-task('cleanTmp', () => del(['tsconfig.tsbuildinfo'], { force: true, dot: true }));
+task('cleanTmp', () => del(['**/tsconfig.tsbuildinfo'], { force: true, dot: true }));
 
 task('cleanNext', () =>
-  del(['.next'], {
-    force: true,
-    dot: true,
-  }),
+	del(['**/dist'], {
+		force: true,
+		dot: true,
+	}),
 );
 
 exports.superclean = parallel('cleanNext', 'cleanTmp', 'cleanModules');
