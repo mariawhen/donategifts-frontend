dfd38d2a4cc77738363f1e14c7193be46444bd0a
Enubia <marcoschuster@mailbox.org>
2021-02-13 22:03:10 +0100

    feat: index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import BaseLayout from '@/Components/Layout/BaseLayout';
 
 export default function Index(): JSX.Element {
 	return (
-		<BaseLayout pageTitle="Donate Gifts">
+		<BaseLayout pageTitle="Holiday Gift Donation | Donate Gifts to Foster Kids -Virtual Toy Drive">
 			<div className="flex w-full justify-center">
 				<div id="home-bg" className="w-full" />
 			</div>
