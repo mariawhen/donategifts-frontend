7bdf65d4831c4d72c2ead0a57dac7c2b4f5037d7
Maria Nguyen <maria@Marias-MacBook-Pro.local>
2021-10-02 15:32:40 -0700

    Wish card component

--- src/components/wishcard/Card.tsx
+++ src/components/wishcard/Card.tsx
@@ -3,6 +3,32 @@
 
 import React from 'react';
 
-export default function Card(): JSX.Element {
-  return <div>Card</div>;
+interface CardProps {
+  cardInfo: {
+    name: string;
+    item: string;
+    price: string;
+    interest: string;
+    location: string;
+  }
+}
+
+export default function Card({ cardInfo }: CardProps): JSX.Element {
+  const { name, item, price, interest, location } = cardInfo;
+
+	return (
+		<div className="card col-span-1 flex rounded-xl">
+			<div className="card__msg-btn">message</div>
+			<div className="card__img">photo</div>
+			<div className="card__info">
+				<div className="name">{name}</div>
+				<div className="wish">{item}</div>
+				<div className="price">{price}</div>
+				<div className="interest">{interest}</div>
+        <div className="interest">{location}</div>
+				<button className="read-more rounded-lg">more</button>
+				<button className="donate rounded-lg">donate</button>
+			</div>
+		</div>
+	);
 }
--- src/pages/wishcards/index.tsx
+++ src/pages/wishcards/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import BaseLayout from '@/components/layout/BaseLayout';
+import BaseLayout from '@/Components/layout/BaseLayout';
+import Card from '@/Components/Wishcard/Card';
+
 
 export async function getStaticProps({ locale }) {
   return {
@@ -10,10 +12,28 @@ export async function getStaticProps({ locale }) {
   };
 }
 
+const mockCards = [
+	{
+		name: "Katy",
+		item: "crayons",
+    price: "25",
+    interest: "skating",
+    location: "somewhere"
+	}
+]
+
 export default function Index(): JSX.Element {
-  return (
-    <BaseLayout pageTitle="Wishcards">
-      <div>Wishcards Page</div>
-    </BaseLayout>
-  );
+	return (
+		<BaseLayout pageTitle="Wishcards">
+			Wishcards Page
+			<div className="row">
+			<div className="grid grid-cols-3 gap-4">
+				<Card cardInfo={mockCards[0]} />
+        <Card cardInfo={mockCards[0]} />
+        <Card cardInfo={mockCards[0]} />
+			</div>
+			</div>
+			
+		</BaseLayout>
+	);
 }
